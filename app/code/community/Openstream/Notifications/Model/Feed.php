0f18af9cd0af1ce41dccaaabcde73570eaaa24f0
tim-bezhashvyly <tim@openstream.ch>
2013-02-27 11:44:19 +0100

    OIS-225: Bugfix
    Add missing getLastUpdate and setLastUpdate methods in order to fix bug with custom feed never get pulled.

--- app/code/community/Openstream/Notifications/Model/Feed.php
+++ app/code/community/Openstream/Notifications/Model/Feed.php
@@ -6,4 +6,15 @@ class Openstream_Notifications_Model_Feed extends Mage_AdminNotification_Model_F
     {
         return (Mage::getStoreConfigFlag(self::XML_USE_HTTPS_PATH) ? 'https://' : 'http://') . Mage::getStoreConfig('system/openstream_notifications/feed_url');
     }
+    
+    public function getLastUpdate()
+    {
+        return Mage::app()->loadCache('openstream_notifications_lastcheck');
+    }
+
+    public function setLastUpdate()
+    {
+        Mage::app()->saveCache(time(), 'openstream_notifications_lastcheck');
+        return $this;
+    }
 }
